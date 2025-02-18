51ef1bf52b382ab2e8e5358ed8ace711a73e2172
Adrian Castro <22133246+castdrian@users.noreply.github.com>
2025-02-18 16:24:40 +0100

    Initial Commit

---
+++ ShareToDiscord/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ShareToDiscord
+//
+//  Created by Adrian Castro on 18/2/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ShareToDiscord/ShareToDiscordApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShareToDiscordApp.swift
+//  ShareToDiscord
+//
+//  Created by Adrian Castro on 18/2/25.
+//
+
+import SwiftUI
+
+@main
+struct ShareToDiscordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
